c3949ea3f7e09b5ad0cee514cc854038eadbef5a
Juyoun Park <juyoun726@gmail.com>
2020-08-10 16:31:09 -0400

    move_robot fixed

--- control/src/move_robot.py
+++ control/src/move_robot.py
@@ -2,14 +2,37 @@
 """Publishes joint trajectory to move robot to given pose"""
 
 import rospy
+from std_msgs.msg import Float64
 from trajectory_msgs.msg import JointTrajectory
 from trajectory_msgs.msg import JointTrajectoryPoint
 from std_srvs.srv import Empty
 import argparse
 import time
 
-def moveJoint (jointcmds,prefix,nbJoints):
-  topic_name = '/' + prefix + '/effort_joint_trajectory_controller/command'
+def moveJointPosition (jointcmds,rname,prefix,nbJoints):
+  for i in range(nbJoints):
+    topic_name = '/' + rname + '/' + prefix + '_joint_' + str(i+1) + '_position_controller/command'
+    pub = rospy.Publisher(topic_name, Float64, queue_size=10)
+    rate = rospy.Rate(100)
+    count = 0
+    while (count < 10):
+      pub.publish(jointcmds[i])
+      count = count + 1
+      rate.sleep()
+
+def moveFingersPosition (jointcmds,rname,prefix,nbJoints):
+  for i in range(nbJoints):
+    topic_name = '/' + rname + '/' + prefix + '_finger_' + str(i+1) + '_position_controller/command'
+    pub = rospy.Publisher(topic_name, Float64, queue_size=10)
+    rate = rospy.Rate(100)
+    count = 0
+    while (count < 10):
+      pub.publish(jointcmds[i])
+      count = count + 1
+      rate.sleep()
+
+def moveJointTrajectory (jointcmds,rname,prefix,nbJoints):
+  topic_name = '/' + rname + '/' + prefix + '_effort_joint_trajectory_controller/command'
   pub = rospy.Publisher(topic_name, JointTrajectory, queue_size=1)
   jointCmd = JointTrajectory()  
   point = JointTrajectoryPoint()
@@ -29,8 +52,8 @@ def moveJoint (jointcmds,prefix,nbJoints):
     count = count + 1
     rate.sleep()     
 
-def moveFingers (jointcmds,prefix,nbJoints):
-  topic_name = '/' + prefix + '/effort_finger_trajectory_controller/command'
+def moveFingersTrajectory (jointcmds,rname,prefix,nbJoints):
+  topic_name = '/' + rname + '/' + prefix + '_effort_finger_trajectory_controller/command'
   pub = rospy.Publisher(topic_name, JointTrajectory, queue_size=1)  
   jointCmd = JointTrajectory()  
   point = JointTrajectoryPoint()
@@ -62,12 +85,12 @@ if __name__ == '__main__':
     resp = unpause_gazebo()
 
     # left arm
-    moveJoint ([1.3,1.07,4.2,-1.6,0.0,0.0],'left',6)
-    moveFingers ([1,1,1],'left',3)
+    moveJointPosition ([1.3,1.07,4.2,-1.6,0.0,0.0],'robot','left',6)
+    moveFingersPosition ([1,1,1],'robot','left',3)
 
 	# right arm
-    moveJoint ([-1.3,5.21,2.08,1.6,0.0,0.0],'right',6)
-    moveFingers ([1,1,1],'right',3)
+    moveJointPosition ([-1.3,5.21,2.08,1.6,0.0,0.0],'robot','right',6)
+    moveFingersPosition ([1,1,1],'robot','right',3)
 
   except rospy.ROSInterruptException:
     print "program interrupted before completion"
